1dacfce856d6015dc2d2ded485f65b807d7c8877
bichngoc97 <bichngoc97.vn@gmail.com>
2018-03-10 09:47:12 +0700

    Kết Nối

---
+++ QuanLyKho/QuanLyKho/DAL/DATA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace QuanLyKho.DAL
+{
+    class DATA
+    {
+        public static DataTable xuat_DS()
+        {
+            return DataProvider.GetData("get_DS");
+
+        }
+    }
+}
