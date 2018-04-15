c763e5da82c531bceab93e85b52cf7cd206318a2
chiyul21 <chiyulsnsd@gmail.com>
2018-04-15 08:40:44 +0700

    ThongKe

---
+++ QuanLyKho/QuanLyKho/GUI/ThongKe.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace QuanLyKho.GUI
+{
+    partial class ThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbxThongKe = new System.Windows.Forms.ComboBox();
+            this.dgvThongKe = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThongKe)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Aqua;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(539, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(113, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(328, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thống Kê Hàng Hoá";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Thống Kê";
+            // 
+            // cbxThongKe
+            // 
+            this.cbxThongKe.FormattingEnabled = true;
+            this.cbxThongKe.Location = new System.Drawing.Point(158, 120);
+            this.cbxThongKe.Name = "cbxThongKe";
+            this.cbxThongKe.Size = new System.Drawing.Size(198, 24);
+            this.cbxThongKe.TabIndex = 2;
+            this.cbxThongKe.SelectedIndexChanged += new System.EventHandler(this.cbxThongKe_SelectedIndexChanged);
+            // 
+            // dgvThongKe
+            // 
+            this.dgvThongKe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThongKe.Location = new System.Drawing.Point(0, 169);
+            this.dgvThongKe.Name = "dgvThongKe";
+            this.dgvThongKe.RowTemplate.Height = 24;
+            this.dgvThongKe.Size = new System.Drawing.Size(539, 150);
+            this.dgvThongKe.TabIndex = 3;
+            // 
+            // ThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 323);
+            this.Controls.Add(this.dgvThongKe);
+            this.Controls.Add(this.cbxThongKe);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "ThongKe";
+            this.Text = "ThongKe";
+            this.Load += new System.EventHandler(this.ThongKe_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThongKe)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbxThongKe;
+        private System.Windows.Forms.DataGridView dgvThongKe;
+    }
+}
